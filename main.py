52722c591963f57978a2f66c0f05e1dfe4a9b454
shuibinlong <dragon60066@163.com>
2021-09-28 19:50:14 +0800

    experiment module init

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import argparse
 from dataset import Dataset
 
-ds = Dataset('JF17K', {'node': 6, 'edge': 10})
-print('relation_num={}, entity_num={}'.format(ds.relation_cnt, ds.entity_cnt))
+class Experiment:
+    def __init__(self, args):
+        self.lr = args.lr
+        self.emb_dim = args.emb_dim
+        self.batch_size = args.batch_size
+        self.neg_ratio = args.nr
+        self.test = args.test
+        self.epochs = args.epochs
+
+        self.max_arity = {'node': args.node, 'edge': args.edge}
+        self.dataset = Dataset(args.dataset, self.max_arity)
+        print('relation_num={}, entity_num={}'.format(self.dataset.relation_cnt, self.dataset.entity_cnt))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    experiment = Experiment(args)
