6dcda408fd1547c208391f81ebb7d9fdce7c30cd
shuibinlong <dragon60066@163.com>
2021-09-29 20:48:43 +0800

    update experiment module & bug fix

--- main.py
+++ main.py
@@ -1,18 +1,65 @@
+import time
+import torch
 import argparse
+import numpy as np
+from models import *
 from dataset import Dataset
 
 class Experiment:
     def __init__(self, args):
         self.lr = args.lr
-        self.emb_dim = args.emb_dim
         self.batch_size = args.batch_size
         self.neg_ratio = args.nr
         self.test = args.test
         self.epochs = args.epochs
-
+        self.device = args.device
+        self.opt = args.opt
+        self.weight_decay = args.weight_decay
         self.max_arity = {'node': args.node, 'edge': args.edge}
+        self.emb_dim = args.emb_dim
+        self.hidden_dim = args.hidden_dim
+        self.alpha = args.alpha
+        self.dropout = args.dropout
+        self.nheads = args.nheads
         self.dataset = Dataset(args.dataset, self.max_arity)
         print('relation_num={}, entity_num={}'.format(self.dataset.relation_cnt, self.dataset.entity_cnt))
+
+        self.node_embs = torch.FloatTensor(np.random.randn(self.dataset.entity_cnt, self.emb_dim)).to(args.device)
+        self.edge_embs = torch.FloatTensor(np.random.randn(self.dataset.relation_cnt, self.emb_dim)).to(args.device)
+    
+    def load_model(self):
+        self.model = HyperGAT(self.node_embs, self.edge_embs, self.max_arity, self.emb_dim, self.hidden_dim, self.emb_dim, self.alpha, self.dropout, self.nheads)
+        if self.opt == 'Adagrad':
+            self.opt = torch.optim.Adagrad(self.model.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        elif self.opt == "Adam":
+            self.opt = torch.optim.Adam(self.model.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        # TODO: model ckpt save & load
+
+    def train_and_eval(self):
+        print('Training the model...')
+        print('Number of training data points: {}'.format(len(self.dataset.data['train'])))
+
+        loss_layer = torch.nn.CrossEntropyLoss()
+        print('Starting training at iteration ...')
+        for epoch in range(self.epochs):
+            self.model.train()
+            epoch_st = time.time()
+            epoch_loss = []
+            
+            num_iterations = len(self.dataset.data['train']) // self.batch_size
+            if len(self.dataset.data['train']) % self.batch_size != 0:
+                num_iterations += 1
+            
+            for it in range(num_iterations):
+                it_st = time.time()
+                batch_data, batch_labels = self.dataset.get_next_batch(self.batch_size, self.neg_ratio, self.device)
+                batch_outputs = self.model(batch_data, self.dataset.edge_list, self.dataset.node_list)
+                # TODO: calc loss & bp
+                self.opt.step()
+                it_ed = time.time()
+                print('Iteration #{}: loss={:.4f}, time={:.4f}'.format(it, 0, it_ed - it_st))
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
--- models.py
+++ models.py
@@ -4,32 +4,36 @@ import torch.nn.functional as F
 from layers import SparseHyperGraphAttentionLayer
 
 class HyperGAT(nn.Module):
-    def __init__(self, max_arity, nfeat, nhid, nemb, alpha, dropout, nheads):
+    def __init__(self, node_embs, edge_embs, max_arity, nfeat, nhid, nemb, alpha, dropout, nheads):
         super().__init__()
         self.emb_dim = nemb
         self.dropout = dropout
+        self.node_embs = nn.Parameter(node_embs)
+        self.edge_embs = nn.Parameter(edge_embs)
+       
         self.attentions = [SparseHyperGraphAttentionLayer(nfeat, nhid, max_arity, alpha, dropout, concat=True)]
         for i, attention in enumerate(self.attentions):
             self.add_module(f'attention_{i}', attention)
         self.out_attention = SparseHyperGraphAttentionLayer(nhid * nheads, nemb, max_arity, alpha, dropout, concat=False)
 
-    def forward(self, batch_inputs, node_embs, edge_embs, edge_list, node_list):
+    def forward(self, batch_inputs, edge_list, node_list):
         node_embs_list, edge_embs_list = [], []
         for attention in self.attentions:
-            x, y = attention(node_embs, edge_embs, edge_list, node_list)
+            x, y = attention(self.node_embs, self.edge_embs, edge_list, node_list)
             node_embs_list.append(x)
             edge_embs_list.append(y)
-        node_embs = torch.cat(node_embs_list, dim=1)
-        node_embs = F.dropout(node_embs, self.dropout, training=self.training)
-        edge_embs = torch.cat(edge_embs_list, dim=1)
-        edge_embs = F.dropout(edge_embs, self.dropout, training=self.training)
+        out_node_embs = torch.cat(node_embs_list, dim=1)
+        out_node_embs = F.dropout(out_node_embs, self.dropout, training=self.training)
+        out_edge_embs = torch.cat(edge_embs_list, dim=1)
+        out_edge_embs = F.dropout(out_edge_embs, self.dropout, training=self.training)
 
-        node_embs, edge_embs = self.out_attention(node_embs, edge_embs, edge_list, node_list)
-        node_embs, edge_embs = F.elu(node_embs), F.elu(edge_embs)
+        out_node_embs, out_edge_embs = self.out_attention(out_node_embs, out_edge_embs, edge_list, node_list)
+        out_node_embs, out_edge_embs = F.elu(out_node_embs), F.elu(out_edge_embs)
 
+        # TODO: move batch data into loss func
         batch_outputs = torch.zeros(batch_inputs.shape).unsqueeze(2).repeat_interleave(self.emb_dim, 2)
-        batch_outputs[:, 0, :] = edge_embs[batch_inputs[:, 0], :]
+        batch_outputs[:, 0, :] = out_edge_embs[batch_inputs[:, 0], :]
         for i in range(len(batch_inputs)):
             right = batch_inputs[i, :-1].nonzero()[0][-1] + 1
-            batch_outputs[i, 1:right, :] = node_embs[batch_outputs[i, 1:right], :]
+            batch_outputs[i, 1:right, :] = out_node_embs[batch_outputs[i, 1:right], :]
         return batch_outputs
