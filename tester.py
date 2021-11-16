aeab4bd0fb631da6898df782c31687bb91546ca3
shuibinlong <dragon60066@163.com>
2021-11-16 09:36:15 +0800

    Bug fixes: Update tqdm in testing enumeration

--- tester.py
+++ tester.py
@@ -117,7 +117,7 @@ class Tester:
         normalizer = 0
         # Contains the measure values for the given dataset (e.g. test for arity 2)
         current_rank = Measure()
-        for i, fact in tqdm(enumerate(dataset)):
+        for i, fact in tqdm(enumerate(dataset), total=len(dataset)):
             arity = self.dataset.max_arity - (fact == 0).sum()
             for j in range(1, arity + 1):
                 normalizer += 1
